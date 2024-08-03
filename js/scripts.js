cd418c7c6ccfc2614cb3055a9c8e4f6ff25299af
Francisco-AMenezes <franciscoalvesmenezes90@gmail.com>
2024-08-03 13:12:25 -0300

    Gerando perguntas de astronomia

--- js/scripts.js
+++ js/scripts.js
@@ -8,133 +8,424 @@ let points = 0;
 let actualQuestion = 0;
 
 // Perguntas
-
 const questions = [
   {
-    question: 'Quem "adotou" o Bastion?:',
+    question: "Qual é a principal componente da atmosfera de Titã, lua de Saturno?",
     answers: [
       {
-        answer: "Torbjörn",
+        answer: "Metano",
+        correct: false,
+      },
+      {
+        answer: "Nitrogênio",
         correct: true,
       },
       {
-        answer: "Reinhardt",
+        answer: "Dióxido de carbono",
         correct: false,
       },
       {
-        answer: "Soujorn",
-        correct: false,
-      },
-      {
-        answer: "Tracer",
-        correct: false,
-      },
-    ],
-  },
-
-  {
-    question: "Qual o nome da Tracer?:",
-    answers: [
-      {
-        answer: "Olivia Colomar",
-        correct: false,
-      },
-      {
-        answer: "Vivian Chase",
-        correct: false,
-      },
-      {
-        answer: "Lena Oxton",
-        correct: true,
-      },
-      {
-        answer: "Amélie Lacroix",
+        answer: "Oxigênio",
         correct: false,
       },
     ],
   },
   {
-    question: "Quem costumava alimentar o Pet da Soujurn com resto de comida?:",
+    question: "Qual é a distância aproximada da Terra ao centro da Via Láctea?",
     answers: [
       {
-        answer: "Reinhardt",
+        answer: "2.6 mil anos-luz",
         correct: false,
       },
       {
-        answer: "Mercy",
+        answer: "8.5 mil anos-luz",
+        correct: false,
+      },
+      {
+        answer: "25 mil anos-luz",
         correct: true,
       },
       {
-        answer: "Ana",
-        correct: false,
-      },
-      {
-        answer: "Soldado: 76",
+        answer: "50 mil anos-luz",
         correct: false,
       },
     ],
   },
   {
-    question: "Quem foi o líder do Monastério de Shambali?:",
+    question: "Qual é o nome do fenômeno que ocorre quando uma estrela massiva colapsa em um buraco negro?",
     answers: [
       {
-        answer: "Zenyatta",
+        answer: "Nova",
         correct: false,
       },
       {
-        answer: "Genji",
-        correct: false,
-      },
-      {
-        answer: "Ramattra",
-        correct: false,
-      },
-      {
-        answer: "Mondatta",
-        correct: true,
-      },
-    ],
-  },
-  {
-    question: "Qual destes personagens possui parentesco com Genji e Hanzo?:",
-    answers: [
-      {
-        answer: "Kiriko",
+        answer: "Supernova",
         correct: true,
       },
       {
-        answer: "Mei",
+        answer: "Pulsar",
         correct: false,
       },
       {
-        answer: "Mercy",
-        correct: false,
-      },
-      {
-        answer: "Tracer",
+        answer: "Quasar",
         correct: false,
       },
     ],
   },
   {
-    question:
-      "Qual destes personagens não habitou a estação espacial internacional?:",
+    question: "Qual é a teoria atualmente mais aceita para a origem do universo?",
     answers: [
       {
-        answer: "Winston",
+        answer: "Teoria do Estado Estacionário",
         correct: false,
       },
       {
-        answer: "Hammond",
-        correct: false,
-      },
-      {
-        answer: "Sigma",
-        correct: false,
-      },
-      {
-        answer: "Tracer",
+        answer: "Teoria do Big Bang",
         correct: true,
+      },
+      {
+        answer: "Teoria da Panspermia",
+        correct: false,
+      },
+      {
+        answer: "Teoria das Cordas",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome da galáxia que está em rota de colisão com a Via Láctea?",
+    answers: [
+      {
+        answer: "Galáxia de Andrômeda",
+        correct: true,
+      },
+      {
+        answer: "Galáxia do Triângulo",
+        correct: false,
+      },
+      {
+        answer: "Galáxia de Centaurus A",
+        correct: false,
+      },
+      {
+        answer: "Galáxia de Sombrero",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome da missão que enviou o primeiro rover para Marte?",
+    answers: [
+      {
+        answer: "Viking",
+        correct: false,
+      },
+      {
+        answer: "Pathfinder",
+        correct: true,
+      },
+      {
+        answer: "Curiosity",
+        correct: false,
+      },
+      {
+        answer: "Opportunity",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é a principal característica que distingue um buraco negro de um buraco de minhoca?",
+    answers: [
+      {
+        answer: "Massa",
+        correct: false,
+      },
+      {
+        answer: "Densidade",
+        correct: false,
+      },
+      {
+        answer: "Horizonte de eventos",
+        correct: true,
+      },
+      {
+        answer: "Conexão entre diferentes pontos do espaço-tempo",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome do método que utiliza a paralaxe estelar para medir distâncias astronômicas?",
+    answers: [
+      {
+        answer: "Método de Trigonometria",
+        correct: true,
+      },
+      {
+        answer: "Método de Espectroscopia",
+        correct: false,
+      },
+      {
+        answer: "Método de Astrometria",
+        correct: false,
+      },
+      {
+        answer: "Método de Fotometria",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome da região ao redor de um buraco negro onde nada pode escapar, nem mesmo a luz?",
+    answers: [
+      {
+        answer: "Singularidade",
+        correct: false,
+      },
+      {
+        answer: "Horizonte de eventos",
+        correct: true,
+      },
+      {
+        answer: "Raio de Schwarzschild",
+        correct: false,
+      },
+      {
+        answer: "Disco de acreção",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome do primeiro exoplaneta confirmado orbitando uma estrela semelhante ao Sol?",
+    answers: [
+      {
+        answer: "51 Pegasi b",
+        correct: true,
+      },
+      {
+        answer: "HD 209458 b",
+        correct: false,
+      },
+      {
+        answer: "Kepler-186f",
+        correct: false,
+      },
+      {
+        answer: "Proxima Centauri b",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome do fenômeno que explica a expansão acelerada do universo?",
+    answers: [
+      {
+        answer: "Matéria escura",
+        correct: false,
+      },
+      {
+        answer: "Energia escura",
+        correct: true,
+      },
+      {
+        answer: "Radiação cósmica de fundo",
+        correct: false,
+      },
+      {
+        answer: "Efeito Doppler",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o período de rotação do pulsar mais rápido conhecido?",
+    answers: [
+      {
+        answer: "1.4 milissegundos",
+        correct: true,
+      },
+      {
+        answer: "2.8 milissegundos",
+        correct: false,
+      },
+      {
+        answer: "4.2 milissegundos",
+        correct: false,
+      },
+      {
+        answer: "6.4 milissegundos",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é a principal teoria sobre a formação da Lua?",
+    answers: [
+      {
+        answer: "Teoria da Captura",
+        correct: false,
+      },
+      {
+        answer: "Teoria da Co-formação",
+        correct: false,
+      },
+      {
+        answer: "Teoria do Impacto Gigante",
+        correct: true,
+      },
+      {
+        answer: "Teoria da Fissão",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é a massa mínima necessária para que uma estrela termine sua vida como um buraco negro?",
+    answers: [
+      {
+        answer: "1.4 massas solares",
+        correct: false,
+      },
+      {
+        answer: "3.0 massas solares",
+        correct: true,
+      },
+      {
+        answer: "8.0 massas solares",
+        correct: false,
+      },
+      {
+        answer: "20.0 massas solares",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é a temperatura aproximada do núcleo do Sol?",
+    answers: [
+      {
+        answer: "1 milhão de Kelvin",
+        correct: false,
+      },
+      {
+        answer: "5 milhões de Kelvin",
+        correct: false,
+      },
+      {
+        answer: "10 milhões de Kelvin",
+        correct: false,
+      },
+      {
+        answer: "15 milhões de Kelvin",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Qual é a principal causa da emissão de radiação gama em explosões de supernovas?",
+    answers: [
+      {
+        answer: "Decaimento radioativo de elementos pesados",
+        correct: true,
+      },
+      {
+        answer: "Fusão de núcleos atômicos",
+        correct: false,
+      },
+      {
+        answer: "Emissão de neutrinos",
+        correct: false,
+      },
+      {
+        answer: "Aniquilação de partículas e antipartículas",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome do telescópio espacial que observou a radiação cósmica de fundo em micro-ondas com alta precisão?",
+    answers: [
+      {
+        answer: "Hubble",
+        correct: false,
+      },
+      {
+        answer: "Kepler",
+        correct: false,
+      },
+      {
+        answer: "Planck",
+        correct: true,
+      },
+      {
+        answer: "Spitzer",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é a principal característica dos buracos negros supermassivos encontrados nos centros das galáxias?",
+    answers: [
+      {
+        answer: "Baixa densidade",
+        correct: false,
+      },
+      {
+        answer: "Alta rotação",
+        correct: false,
+      },
+      {
+        answer: "Alta luminosidade",
+        correct: false,
+      },
+      {
+        answer: "Massa superior a um milhão de massas solares",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome do fenômeno que causa a curvatura da luz ao passar por um campo gravitacional intenso?",
+    answers: [
+      {
+        answer: "Desvio para o vermelho gravitacional",
+        correct: false,
+      },
+      {
+        answer: "Lente gravitacional",
+        correct: true,
+      },
+      {
+        answer: "Efeito Doppler",
+        correct: false,
+      },
+      {
+        answer: "Efeito Compton",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual é o nome da missão espacial que trouxe amostras de asteroides para a Terra?",
+    answers: [
+      {
+        answer: "Voyager",
+        correct: false,
+      },
+      {
+        answer: "New Horizons",
+        correct: false,
+      },
+      {
+        answer: "OSIRIS-REx",
+        correct: true,
+      },
+      {
+        answer: "Juno",
+        correct: false,
       },
     ],
   },
@@ -224,7 +515,7 @@ function nextQuestion() {
   setTimeout(function () {
     //verifica se já está na última pergunta
     if (actualQuestion >= questions.length) {
-      // aperesenta a mensagem de sucesso
+      // apresenta a mensagem de sucesso
       showSuccessMessage();
     }
 
@@ -244,7 +535,7 @@ function showSuccessMessage() {
 
   displayScore.textContent = score.toString();
 
-  //aterar o número de perguntas corretas
+  //alterar o número de perguntas corretas
   const correctAnswers = document.querySelector("#correct-answers");
 
   correctAnswers.textContent = points;
@@ -254,13 +545,12 @@ function showSuccessMessage() {
   questionsQuantity.textContent = questions.length
 }
 
-
+//Reinicia o Quizz
 const restartButton = document.querySelector('#restart')
 
 restartButton.addEventListener('click', function() {
     location.reload()
 })
-
 
 // Inicializando o Quizz
 init();
